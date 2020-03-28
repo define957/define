e110bc728f43656524e89fe45c81bd2f1f76b13a
define957 <53004538+define957@users.noreply.github.com>
2020-03-28 21:00:48 +0800

    Update distribute_graph.py

--- distribute_graph.py
+++ distribute_graph.py
@@ -13,17 +13,36 @@ def f_distribution_graph(m, n, ub, label=None, color=None,legend_position='best'
     y = stats.f.pdf(x, m, n)
     plt.plot(x, y, label=label, c=color)
     plt.xlim(0,ub)
-    plt.legend('best')
+    plt.legend()
 
 
 def t_distribution_graph(n: int, lb: float, ub: float, label=None, color=None) -> None:
     """
-    m: degree of freedom m
     n:degree of freedom n
     lb and ub : lower bound and upper bound
+    label:the label of graph
     """
     x = np.linspace(lb, ub)
     y = stats.t.pdf(x, n)
     plt.plot(x, y, label=label, c=color)
-    plt.xlim(0,ub)
-    plt.legend('best')
+    plt.xlim(lb,ub)
+    plt.legend()
+
+
+def chi2_distribution_graph(n: int, ub: float, label=None, color=None) -> None:
+    """
+
+    :param n:  degree of freedom
+    :param ub: graph upper bound
+    :param label: name of line
+    :param color: color of line
+    :return: None
+    """
+    x = np.linspace(0, ub)
+    y = stats.chi2.pdf(x, n)
+    plt.plot(x, y, label=label, c=color)
+    plt.xlim(0, ub)
+    plt.legend()
+
+
+
