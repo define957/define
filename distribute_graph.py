ba62392e0b0793bab7f31c2a108e0205211d25ba
define957 <53004538+define957@users.noreply.github.com>
2020-03-28 19:26:22 +0800

    Update distribute_graph.py

--- distribute_graph.py
+++ distribute_graph.py
@@ -2,21 +2,28 @@ import numpy as np
 from scipy import stats as stats
 import matplotlib.pyplot as plt
 
-def show
 
-def f_distribution_graph(m, n, ub, label=None, color=None,legend_position='best'):
+def f_distribution_graph(m, n, ub, label=None, color=None,legend_position='best') -> None:
     """
     m: degree of freedom m
     n:degree of freedom n
     ub: x-axis limit
-    legend_position: ''
     """
     x = np.linspace(0, ub, 1000)
     y = stats.f.pdf(x, m, n)
     plt.plot(x, y, label=label, c=color)
     plt.xlim(0,ub)
-    plt.legend()
-    plt.legend()
+    plt.legend('best')
 
 
-def
+def t_distribution_graph(n: int, lb: float, ub: float, label=None, color=None) -> None:
+    """
+    m: degree of freedom m
+    n:degree of freedom n
+    lb and ub : lower bound and upper bound
+    """
+    x = np.linspace(lb, ub)
+    y = stats.t.pdf(x, n)
+    plt.plot(x, y, label=label, c=color)
+    plt.xlim(0,ub)
+    plt.legend('best')
